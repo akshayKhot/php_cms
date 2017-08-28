4f2a98c959152b9733b79d07f92d98f8e2745b1d
Akshay Khot <akshay.7277@gmail.com>
2017-08-27 21:56:23 -0700

    add icons dynamically

--- php/fetch.php
+++ php/fetch.php
@@ -4,7 +4,7 @@
         require 'connect.php';
 
         $user_id = $_SESSION['user_id'];
-        $posts_query = "SELECT * FROM posts WHERE author_id='$user_id';";
+        $posts_query = "SELECT * FROM posts WHERE author_id='$user_id' ORDER BY date DESC;";
         $posts_result = $db->query($posts_query) or die($db->error);
         
         $query = "SELECT name FROM users WHERE user_id='" . $user_id . "';";
@@ -15,7 +15,7 @@
             createPost($post, $user);
         }
     } else {
-        $posts_query = "SELECT * FROM posts;";
+        $posts_query = "SELECT * FROM posts ORDER BY date DESC;";
         $posts_result = $db->query($posts_query) or die($db->error);
 
         foreach ($posts_result as $post) {
@@ -30,9 +30,16 @@
     }
 
     function createPost($post, $user) {
+
+        if(isset($_SESSION['user_id'])) {
+            $icons = "<span class='deletePost'><i class='fa fa-times' aria-hidden='true'></i></span><span class='editPost'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></span>";
+        } else {
+            $icons = "";
+        }
+
         echo "<article>";
         echo "<h2><a href='php/post.php?id=".$post["post_id"]."&author=".$user["name"]."'>" . $post['title'] . "</a></h2>";
-        echo "<div class='author'>-" . $user['name'] . "</div>";
+        echo "<div class='author'>-" . $user['name'] . $icons . "</div>";
         echo "</article>";
     }
 ?>
--- php/header.php
+++ php/header.php
@@ -6,10 +6,11 @@
    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
    <link rel='stylesheet' href='http://localhost:8888/php_cms/style.css'>
    <script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
-   </head>
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/f688c6e504.js"></script>
+</head>
 <body>
 
    <div class="container">
