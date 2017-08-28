59ca9c27df13e3a383087d84e137aa5b229ffcb3
Akshay Khot <akshay.7277@gmail.com>
2017-08-27 23:25:02 -0700

    implement update features

--- php/modifyPost.php
+++ php/modifyPost.php
@@ -1,17 +1,60 @@
 <?php 
     include "common.php";
-    require 'connect.php';
+    require "connect.php";
     if(isset($_SESSION["user_id"])) {
         if(isset($_GET["deletePost"])) {
             $deleteQuery = "DELETE FROM posts WHERE post_id=$_GET[deletePost]";
             $db->query($deleteQuery) or die($db->error);
             header("Location: http://localhost:8888/php_cms/");
         }
-        if(isset($_GET["editPost"])) {
-            echo "editing post $_GET[editPost]";
-
+        elseif(isset($_POST["submitBtn"])) {
+            $updateQuery = "UPDATE posts SET title='$_POST[title]', date='$_POST[date]', content='$_POST[content]' WHERE post_id=$_GET[editPost];";
+            $db->query($updateQuery) or die($db->error);
+            header("Location: http://localhost:8888/php_cms/");
         }
+        elseif(isset($_POST["cancelBtn"])) {
+            header("Location: http://localhost:8888/php_cms/");
+        }
+        elseif(isset($_GET["editPost"])) {
+            include "header.php";
+            $postQuery = "SELECT title, date, content FROM posts WHERE post_id='$_GET[editPost])';";
+            $result = $db->query($postQuery) or die($db->error);
+            $row = mysqli_fetch_assoc($result);
+            $phpdate = strtotime( $row['date'] );   
+?>
 
+<h1 class="text-center spaced">Update Post</h1>
+        <form class="form-horizontal" action="" method="post">
+            <div class="form-group">
+                <label for="titleInput" class="col-sm-2 control-label">Title</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="titleInput" value="<?php echo $row['title']; ?>" name="title">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="dateInput" class="col-sm-2 control-label">Date</label>
+                <div class="col-sm-10">
+                <input type="date" class="form-control" id="dateInput" value="<?php echo date("Y-m-d", $phpdate); ?>" name="date">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="contentInput" class="col-sm-2 control-label">Content</label>
+                <div class="col-sm-10">
+                <textarea type="text" class="form-control" id="contentInput" rows="10" name="content"><?php echo $row['content']?></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10 text-center">
+                <button type="submit" name="submitBtn" class="btn btn-success">Update</button>
+                <button name="cancelBtn" class="btn btn-danger">Cancel</button>
+                </div>
+            </div>
+        </form>
+   
+
+
+<?php
+        }
     }
     include "footer.php";
 ?>
